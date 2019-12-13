9d1920c99af00367d2bdf441d54d0cd759e4c406
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-12-13 20:19:49 +0300

    Add files via upload

---
+++ hw2/HW2.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.lang.NullPointerException;
+import java.util.Scanner;
+
+
+public class HW2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HashMap<String, Integer> dictionary = new HashMap<>();
+
+        //String str = "a bb a bbbb a bb bbbb a aa";
+        String str = in.nextLine();
+        String[] words = str.split(" ");
+        System.out.println(Arrays.toString(words));
+        int value;
+        for (int i = 0; i < words.length; i++) {
+            //value = dictionary.get(words[i]);
+            if (dictionary.get(words[i]) != null) {
+                value = dictionary.get(words[i]);
+                dictionary.put(words[i], value + 1);
+            } else {
+                dictionary.put(words[i], 1);
+            }
+        }
+        //System.out.println(dictionary.get("c"));
+        System.out.println(dictionary);
+        //for (dictionary)
+    }
+}
