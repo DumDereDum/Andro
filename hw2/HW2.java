93ae36c04cc5578f96788b1ca25631a43b7830b5
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2020-01-28 11:16:10 +0300

    Add files via upload

--- hw2/HW2.java
+++ hw2/HW2.java
@@ -1,26 +1,33 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class HW2 {
+
+    public static HashMap<String, Integer> countWords(String str) {
+        HashMap<String, Integer> dictionary = new HashMap<>();
+        Integer value;
+        for (String word : str.split(" ")) {
+            value = dictionary.get(word);
+            if (value != null) {
+                dictionary.put(word, value + 1);
+            } else {
+                dictionary.put(word, 1);
+            }
+        }
+        return dictionary;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        HashMap<String, Integer> dictionary = new HashMap<>();
 
         //String str = "a bb a bbbb a bb bbbb a aa";
         String str = in.nextLine();
         String[] words = str.split(" ");
-        System.out.println(Arrays.toString(words));
-        Integer value;
-        for (int i = 0; i < words.length; i++) {
-            value = dictionary.get(words[i]);
-            if (value != null) {
-                dictionary.put(words[i], value + 1);
-            } else {
-                dictionary.put(words[i], 1);
-            }
-        }
+
+        HashMap<String, Integer> dictionary;
+        dictionary = countWords(str);
+
         System.out.println(dictionary);
     }
 }
